c0ec52553ee660ee6123c7a0d3e85b3e37131b6c
buffalorob <peckham.rob@gmail.com>
2019-01-08 14:52:26 -0500

    expiramental code playing with react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+const PageOne = () => {
+  return (
+    <div>
+      Page One
+      <a href="/pagetwo">Nav to 2</a>
+    </div>
+  );
+};
+
+const PageTwo = () => {
+  return (
+    <div>
+      Page Two
+      <a href="/">Nav to 1</a>
+    </div>
+  );
+};
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path='/' exact component={PageOne} />
+          <Route path='/pagetwo' component={PageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  )
 };
 
 export default App;
