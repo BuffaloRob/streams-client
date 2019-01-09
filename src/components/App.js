7ce47c944f52ae7a2d01fe17753abce018bbff76
buffalorob <peckham.rob@gmail.com>
2019-01-09 10:31:48 -0500

    Moved Header into BrowserRouter to remove error

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamDelete from './streams/StreamDelete';
 import StreamEdit from './streams/StreamEdit';
@@ -8,20 +8,18 @@ import StreamShow from './streams/StreamShow';
 import Header from './Header';
 
 const App = () => {
-  return (
-    <div className='ui container'>
-      <Header />
+  return <div className="ui container">
       <BrowserRouter>
         <div>
-          <Route path='/' exact component={StreamList} />
-          <Route path='/streams/delete' exact component={StreamDelete} />
-          <Route path='/streams/show' exact component={StreamShow} />
-          <Route path='/streams/new' component={StreamCreate} />
-          <Route path='/streams/edit' component={StreamEdit} />
+          <Header />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
+          <Route path="/streams/new" component={StreamCreate} />
+          <Route path="/streams/edit" component={StreamEdit} />
         </div>
       </BrowserRouter>
-    </div>
-  )
+    </div>;
 };
 
 export default App;
