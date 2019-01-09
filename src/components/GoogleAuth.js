7c7cdd8d522814f4747ca7803e76463349c72f05
buffalorob <peckham.rob@gmail.com>
2019-01-09 15:46:10 -0500

    GoogleAuth: + sign in/out detection

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,19 +1,35 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+
+  state = { isSignedIn: null };
+
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
       window.gapi.client.init({
         clientId: '528593862772-3lprfhufo73p69igcp9anli4hjli32ee.apps.googleusercontent.com',
         scope: 'email'
+      }).then(() => {
+        this.auth = window.gapi.auth2.getAuthInstance();
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
       });
     });
+  }
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I don't know if we're signed in</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in</div>;
+    } else {
+      return <div>I am not signed in</div>
+    }
   }
 
   render() {
     return (
       <div>
-        GoogleAuth
+        {this.renderAuthButton()}
       </div>
     );
   }
