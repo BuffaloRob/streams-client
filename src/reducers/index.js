ac14e3ed1bfa7d5430e10a363fc21ab8be5bdf7f
buffalorob <peckham.rob@gmail.com>
2019-01-11 13:49:27 -0500

    added streamReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import authReducer from './authReducer';
+import streamReducer from './streamReducer';
 
 export default combineReducers({
   auth: authReducer,
   //have to use form as key
-  form: formReducer
+  form: formReducer,
+  streams: streamReducer
 })
