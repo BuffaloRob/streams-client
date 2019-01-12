664c306ed0cc08de93ebb0e269a6ee2a03f13224
BuffaloRob <peckham.rob@gmail.com>
2019-01-12 07:55:47 -0500

    StreamList: + edit/delete buttons only appear if user logged in

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -7,19 +7,27 @@ class StreamList extends React.Component {
     this.props.fetchStreams();
   }
 
+  renderAdmin(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <button className='ui button primary'>Edit</button>
+          <button className='ui button negative'>Delete</button>
+        </div>
+      );
+    }
+  }
+
   renderList() {
     return this.props.streams.map(stream => {
-      return (
-        <div className='item' key={stream.id}>
-          <i className='large middle aligned icon camera' />
-          <div className='content'>
+      return <div className="item" key={stream.id}>
+          {this.renderAdmin(stream)}
+          <i className="large middle aligned icon camera" />
+          <div className="content">
             {stream.title}
-            <div className='description'>
-              {stream.description}
-            </div>
+            <div className="description">{stream.description}</div>
           </div>
-        </div>
-      )
+        </div>;
     })
   }
   render() {
@@ -35,7 +43,10 @@ class StreamList extends React.Component {
 }
 
 const mapStateToProps = state => {
-  return { streams: Object.values(state.streams) };
+  return { 
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId,
+  };
 }
 
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
