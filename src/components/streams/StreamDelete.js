102131a55aa88f7f2a533f686e9dce94cfb8fde6
buffalorob <peckham.rob@gmail.com>
2019-01-14 14:28:09 -0500

    StreamDelete: changed to class based & +componentDidMount

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,27 +1,37 @@
 import React from "react";
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 import Modal from '../Modal';
 import history from '../../history';
 
-const StreamDelete = () => {
+class StreamDelete extends React.Component {
 
-  const actions = (
-    <>
-      <button className='ui button negative'>Delete</button>
-      <button className='ui button'>Cancel</button>
-    </>
-  );
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
 
-  return (
-    <div>
-      StreamDelete
-      <Modal 
-        title='Delete Stream'
-        content='Are you you want to delete this stream'
-        actions={actions}
-        onDismiss={() => history.push('/')}
-      />
-    </div>
-  )
-};
+  renderActions() {
+    return (
+      <>
+        <button className='ui button negative'>Delete</button>
+        <button className='ui button'>Cancel</button>
+      </>
+    );
+  }
 
-export default StreamDelete;
+  render() {
+    return (
+      <div>
+        StreamDelete
+        <Modal
+          title='Delete Stream'
+          content='Are you sure you want to delete this stream'
+          actions={this.renderActions()}
+          onDismiss={() => history.push('/')}
+        />
+      </div>
+    );
+  }
+}
+
+export default connect(null, { fetchStream })(StreamDelete);
